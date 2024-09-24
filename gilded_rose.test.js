652e4e1af51b733fa44c4412bec38b5ec5bdbc9a
David Odeyinka <david.odeyinka@yahoo.com>
2024-09-24 19:49:00 -0400

    refactor: Write test for backstage passes

--- gilded_rose.test.js
+++ gilded_rose.test.js
@@ -17,4 +17,12 @@ describe('Gilded Rose', function() {
         expect(item.sell_in).to.equal(9);      // Sell_in should decrease by 1
         expect(item.quality).to.equal(21);     // Quality should increase by 1
     });
+    // Test quality increase for Backstage passes as concert approaches
+    it('should increase quality for Backstage passes when sell_in is 10 or less', function() {
+        const item = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20);
+        const updater = createItemUpdater(item);
+        updater.updateQuality();
+        expect(item.quality).to.equal(22);     // Quality should increase by 2 (10 days or less)
+    });
+
 });
