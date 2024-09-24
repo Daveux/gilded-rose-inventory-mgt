349b475c7834434e0d9145ddf326b5b928322447
David Odeyinka <david.odeyinka@yahoo.com>
2024-09-24 19:48:08 -0400

    refactor: Write test for aged brie

--- gilded_rose.test.js
+++ gilded_rose.test.js
@@ -9,4 +9,12 @@ describe('Gilded Rose', function() {
         expect(item.sell_in).to.equal(9);      // Sell_in should decrease by 1
         expect(item.quality).to.equal(19);     // Quality should decrease by 1
     });
+    // Test that Aged Brie increases in quality
+    it('should increase quality for Aged Brie', function() {
+        const item = new Item('Aged Brie', 10, 20);
+        const updater = createItemUpdater(item);
+        updater.updateQuality();
+        expect(item.sell_in).to.equal(9);      // Sell_in should decrease by 1
+        expect(item.quality).to.equal(21);     // Quality should increase by 1
+    });
 });
