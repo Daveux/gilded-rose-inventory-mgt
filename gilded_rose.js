a5a8e4c064e2e510e62365c8286653fd5641a3a2
David Odeyinka <david.odeyinka@yahoo.com>
2024-09-24 18:48:14 -0400

    refactor: Specify default behaviour for regular items

--- gilded_rose.js
+++ gilded_rose.js
@@ -13,3 +13,29 @@ var items = [
     new Item('Conjured Mana Cake', 3, 6),
     new Item('normal item', 5, 10)
 ];
+class ItemUpdater {
+    constructor(item) {
+        this.item = item;
+    }
+
+    // Default behavior for regular items
+    updateQuality() {
+        if (this.item.quality > 0) {
+            this.item.quality -= 1; // Decrease quality
+        }
+        this.item.sell_in -= 1; // Decrease sell_in
+
+        // If sell_in is below 0, quality decreases twice as fast
+        if (this.item.sell_in < 0 && this.item.quality > 0) {
+            this.item.quality -= 1;
+        }
+
+        this.ensureQualityBounds();
+    }
+
+    // Ensure that quality is between 0 and 50
+    ensureQualityBounds() {
+        if (this.item.quality < 0) this.item.quality = 0;
+        if (this.item.quality > 50) this.item.quality = 50;
+    }
+}
